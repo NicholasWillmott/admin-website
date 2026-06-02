689ed9cc06e8449ab4584182bc1216706cb5ed79
Nicholas Willmott <nicholaswillmottse@gmail.com>
2026-06-02 16:52:25 +0800

    deploy

--- src/frontend/services.ts
+++ src/frontend/services.ts
@@ -851,7 +851,7 @@ export async function exportIndicatorsCsvApi(serverIds: string[], token: string 
     const url = window.URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
-    a.download = `indicators-${new Date().toISOString().slice(0, 10)}.csv`;
+    a.download = `indicators-${new Date().toISOString().slice(0, 10)}.xlsx`;
     document.body.appendChild(a);
     a.click();
     window.URL.revokeObjectURL(url);
